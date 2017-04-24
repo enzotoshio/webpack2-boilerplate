3640c2fc1aba7267cd169ddb21de661c8d32ec7b
Enzo Toshio <enzo.toshiba@gmail.com>
2017-04-24 11:32:33 -0300

    fixing generators transpilation

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,22 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: { filename: 'bundle.js', path: 'dist' },
+  entry: ['babel-polyfill', './src/index.js'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  devServer: {
+    contentBase: './src'
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-        options: { 
+        options: {
           presets: [
             'es2017',
             [
